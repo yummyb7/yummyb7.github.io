b8582e203f4621ca414d15abb7578922cac825fa
yummyb7 <50021558+yummyb7@users.noreply.github.com>
2019-04-27 17:30:41 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,171 @@
+$(function(){
+   
+    $('.slider').slick();
+    
+});
+
+    
+    
+
+    $('.open_popup').click(function(){
+       $('.popup_plisse').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.popup_plisse').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.popup_plisse').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+$('.open_popup2').click(function(){
+       $('.popup_rulon').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.popup_rulon').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.popup_rulon').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+$('.open_popup3').click(function(){
+       $('.popup_vertical').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.popup_vertical').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.popup_vertical').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+$('.open_popup4').click(function(){
+       $('.popup_gorizontal').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.popup_gorizontal').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.popup_gorizontal').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+$('.popup_btng').click(function(){
+       $('.fields').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+
+$('.popup_btnv').click(function(){
+       $('.fields').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+$('.popup_btnr').click(function(){
+       $('.fields').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+$('.popup_btn').click(function(){
+       $('.fields').fadeIn();
+        $('.bg_popup').fadeIn();
+        return false;
+    });
+    
+    $('.popup_close').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+    $('.bg_popup').click(function(){
+        $('.fields').fadeOut();
+        $('.bg_popup').fadeOut();
+        return false;
+    
+    });
+
+
