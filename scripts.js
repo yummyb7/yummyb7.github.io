3f8885f6ad38d1398ea45e6ec2ec06f9e3f89ffc
yummyb7 <50021558+yummyb7@users.noreply.github.com>
2019-05-03 13:07:24 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -168,4 +168,24 @@ $('.popup_btn').click(function(){
     
     });
 
+   $(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
 
